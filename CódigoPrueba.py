7e2384665b3d1578d77e1e6ba0d7042dfdf18afe
“alejandro-cohen” <cohen-7@ciencias.unam.mx>
2021-10-07 21:57:23 -0500

    Estoy haciendo la primera prueba

---
+++ CódigoPrueba.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  7 21:29:11 2021
+
+@author: Universidad
+"""
+
+#Este es sólo un archivo para hacer la prueba de subir a Git Hub
+
+print("sí sólo se imprime esta línea es que no se modifico")
